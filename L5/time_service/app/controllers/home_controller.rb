953cfa80dbcb1d4b84563836a3c42a33538e6b24
cybdoom <enarovlyansky@heliostech.hk>
2013-05-13 17:44:45 +0300

    Added L5/time_service project. Implementation is not finished yet

---
+++ L5/time_service/app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def kiev
+  end
+  def paris
+  end
+  def hong_kong
+  end
+end
---
+++ L5/time_service/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeService::Application.config.secret_token = 'f2b9a2d77bd34e25426c39ccd237f5ba4e16a363bb5c8ba75e1d94db85da6c2fd4dd46260d45f94c20033ed315ececbd4f0d8f54448d45256ec5edb34dd37307'
